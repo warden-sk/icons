7cd7cf3ff054a8abe4dd7e58b277db7a91ba6135
Marek Kobida <marek.kobida@gmail.com>
2021-11-14 19:01:35 +0100

    update

--- icon/sun.js
+++ icon/sun.js
@@ -4,12 +4,13 @@
 
 const { createIcon } = require('../createIcon');
 
-const END = false;
+const END = true;
+const STROKE = false;
 
 createIcon('sun', addPath => {
   addPath((path, { p, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(17, 12));
     path.arcTo(x() - 10, y(), 5, 5, 0);
     path.arcTo(x() + 10, y(), 5, 5, 0);
@@ -17,8 +18,8 @@ createIcon('sun', addPath => {
 
   // up
   addPath((path, { p, vR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(11, 2));
     path.arcTo(x() + 2, y(), 1, 1, 0, 1);
     path.lineTo(vR(2));
@@ -29,8 +30,8 @@ createIcon('sun', addPath => {
 
   // down
   addPath((path, { p, vR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(11, 20));
     path.arcTo(x() + 2, y(), 1, 1, 0, 1);
     path.lineTo(vR(2));
@@ -41,26 +42,26 @@ createIcon('sun', addPath => {
 
   // left
   addPath((path, { hR, p, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
-    path.moveTo(p(2, 13));
-    path.arcTo(x(), y() - 2, 1, 1, 0, 1);
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
+    path.moveTo(p(2, 11));
+    path.arcTo(x(), y() + 2, 1, 1, 1);
     path.lineTo(hR(2));
-    path.arcTo(x(), y() + 2, 1, 1, 0, 1);
+    path.arcTo(x(), y() - 2, 1, 1, 1);
 
-    END && path.lineTo(p(2, 13));
+    END && path.lineTo(p(2, 11));
   });
 
   // right
   addPath((path, { hR, p, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
-    path.moveTo(p(20, 13));
-    path.arcTo(x(), y() - 2, 1, 1, 0, 1);
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
+    path.moveTo(p(20, 11));
+    path.arcTo(x(), y() + 2, 1, 1, 1);
     path.lineTo(hR(2));
-    path.arcTo(x(), y() + 2, 1, 1, 0, 1);
+    path.arcTo(x(), y() - 2, 1, 1, 1);
 
-    END && path.lineTo(p(20, 13));
+    END && path.lineTo(p(20, 11));
   });
 
   const A = 1.415;
@@ -68,8 +69,8 @@ createIcon('sun', addPath => {
 
   // up left
   addPath((path, { p, pR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(4.575 + A, 4.575)); // ?
     path.arcTo(x() - A, y() + A, 1, 1, 1);
     path.lineTo(pR(B, B));
@@ -80,8 +81,8 @@ createIcon('sun', addPath => {
 
   // up right
   addPath((path, { p, pR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(18.0108, 4.575)); // ?
     path.arcTo(x() + A, y() + A, 1, 1, 0, 1);
     path.lineTo(pR(-B, B));
@@ -92,8 +93,8 @@ createIcon('sun', addPath => {
 
   // down left
   addPath((path, { p, pR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(5.636, 16.9497)); // ?
     path.arcTo(x() + A, y() + A, 1, 1, 0, 1);
     path.lineTo(pR(-B, B));
@@ -104,8 +105,8 @@ createIcon('sun', addPath => {
 
   // down right
   addPath((path, { p, pR, x, y }) => {
-    path.strokeColor = '#f00';
-    path.strokeWidth = 0.25;
+    STROKE && (path.strokeColor = '#f00');
+    STROKE && (path.strokeWidth = 0.25);
     path.moveTo(p(16.9497 + A, 16.9497)); // ?
     path.arcTo(x() - A, y() + A, 1, 1, 1);
     path.lineTo(pR(B, B));
