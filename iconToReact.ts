8b565b26b762418d7b3f70a0b87f58d53518d7ec
Marek Kobida <marek.kobida@gmail.com>
2022-04-04 22:02:29 +0200

    update

--- iconToReact.ts
+++ iconToReact.ts
@@ -7,7 +7,10 @@ import IconStorage from './IconStorage';
 function iconToReact({ name, svg }: typeof IconStorage.icons[number]): string {
   svg = svg
     .replace(/(<svg)/, svg => `${svg} {...$}`)
-    .replace(/height="[^"]+"/, `className={className ? \`icon \${className}\` : 'icon'} height={size}`)
+    .replace(
+      /height="[^"]+"/,
+      `className={className ? \`icon \${className}\` : 'icon'} height={size} style={{ minHeight: size, minWidth: size }}`
+    )
     .replace(/width="[^"]+"/, 'width={size}');
 
   return `export const ${name} = ({ className, size = 24, ...$ }: B<JSX.IntrinsicElements['svg']> & { size?: number }) => ${svg};`;
