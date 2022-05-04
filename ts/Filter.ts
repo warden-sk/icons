59d63da1e7747b3240a325602cf90ecbc147f091
Marek Kobida <marek.kobida@gmail.com>
2022-05-04 14:45:41 +0200

    update

---
+++ ts/Filter.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2022 Marek Kobida
+ */
+
+import createIcon from '../createIcon';
+import storage from '../storage';
+
+createIcon('Filter', addPath => {
+  addPath(({ cubicCurveTo, lineTo, moveTo }, { h, hR, p, pR, v, vR }) => {
+    moveTo(p(14, 18));
+    lineTo(hR(-4));
+    lineTo(vR(-2));
+    lineTo(hR(4));
+    lineTo(vR(2));
+    moveTo(p(3, 8));
+    lineTo(vR(-2));
+    lineTo(hR(18));
+    lineTo(vR(2));
+    lineTo(hR(-18));
+    moveTo(p(18, 13));
+    lineTo(hR(-12));
+    lineTo(vR(-2));
+    lineTo(hR(12));
+    lineTo(vR(2));
+  });
+});
