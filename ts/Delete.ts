7d59a922a19536d553e2da68407fec6ee6eb021c
Marek Kobida <marek.kobida@gmail.com>
2022-09-27 20:05:22 +0200

    up

--- ts/Delete.ts
+++ ts/Delete.ts
@@ -7,10 +7,18 @@ import createIcon from '../createIcon';
 
 createIcon('Delete', addPath => {
   addPath(({ cubicCurveTo, lineTo, moveTo }, { h, hR, p, pR, v, vR }) => {
-    moveTo(p(5, 13));
-    lineTo(hR(14));
-    lineTo(vR(-2));
-    lineTo(hR(-14));
-    lineTo(vR(2));
+    moveTo(p(19, 6.41));
+    lineTo(pR(-1.41, -1.41));
+    lineTo(pR(-5.59, 5.59));
+    lineTo(pR(-5.59, -5.59));
+    lineTo(pR(-1.41, 1.41));
+    lineTo(pR(5.59, 5.59));
+    lineTo(pR(-5.59, 5.59));
+    lineTo(pR(1.41, 1.41));
+    lineTo(pR(5.59, -5.59));
+    lineTo(pR(5.59, 5.59));
+    lineTo(pR(1.41, -1.41));
+    lineTo(pR(-5.59, -5.59));
+    lineTo(pR(5.59, -5.59));
   });
 });
